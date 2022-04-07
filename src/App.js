e936e258d01ae5aa29ba97ecabee50dbaedbd485
TriptoAfsin <AfsinTripto@gmail.com>
2022-04-07 21:35:30 +0600

    removed animated routes from app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import { mode } from "@chakra-ui/theme-tools";
-import { BrowserRouter as Router} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ScrollToTopHandler from "./utils/hooks/ScrollToTopHandler";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
-import AnimatedRoutes from "./routes/AnimatedRoutes";
+import FrontPage from "./pages/Home/FrontPage";
+import ContactPage from "./pages/ContactPage";
+import NotFoundPage from "./pages/NotFoundPage";
+import SkillsPage from "./pages/Skills";
 
 function App() {
   console.count("app renderd");
@@ -29,9 +32,18 @@ function App() {
         })}
       >
         <Router>
-        <Navbar />
+          <Navbar />
           <ScrollToTopHandler />
-          <AnimatedRoutes />
+          <Routes>
+            <Route path="/" exact element={<FrontPage />}></Route>
+            <Route path="/cv" exact element={<FrontPage />}></Route>
+            <Route path="/projects" exact element={<FrontPage />}></Route>
+            <Route path="/contact" exact element={<ContactPage />}></Route>
+            <Route path="/skills" exact element={<SkillsPage />}></Route>
+            <Route path="/chatraChatExpanded" element={<FrontPage />}></Route>
+            {/* /*404 route*/}
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
         </Router>
         <Footer />
       </ChakraProvider>
